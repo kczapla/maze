d1a5a9871b6884b6b55f0e26c7106696968fa980
Czapla, Krzysztof <krzysztof.czapla@intel.com>
2019-08-06 14:13:32 +0200

    use grid as maze structure

--- src/maze/maze.hpp
+++ src/maze/maze.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <memory>
 #include <vector>
+#include <utility>
+
+#include "grid.hpp"
 #include "room.hpp"
 
 
@@ -9,11 +12,16 @@ namespace maze
     class Maze
     {
     public:
+        Maze(size_t size = default_maze_size) : _maze(default_maze_size, default_maze_size) {};
         void add_room(std::shared_ptr<Room> room);
         std::shared_ptr<Room> room_no(int room_id) const;
 
+        static constexpr int default_maze_size = 10;
+
     private:
-        std::vector<std::shared_ptr<Room>> _maze;
-        int room_counter = 0;
+        std::pair<int, int> get_room_ords(int room_id);
+        std::vector<std::shared_ptr<Room>> _rooms;
+        Grid<std::shared_ptr<Room>> _maze;
+        int _room_counter = 0;
     };
 }
